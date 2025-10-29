6ffd5d1ce8e4b46fdec13e0618308e358c2d3e94
rosalio-morales <rosaliomorales1975@gmail.com>
2025-10-29 01:11:27 -0600

    Update fetch URL for backend submission

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.getElementById('contactForm').addEventListener('submit', async (e) => {
 
   // Enviar a backend
   try {
-    const response = await fetch('https://landing-page-v10.netlify.app/submit', {
+    const response = await fetch('https://landing-page-v1-57bcbd97af1b.herokuapp.com/submit', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ name, email, message })
